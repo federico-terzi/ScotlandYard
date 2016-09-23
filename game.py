c4fb7a4dc83cdeca4f4cdcd3b19ee953627e609a
LorenzSelv <selvatici.lorenzo@gmail.com>
2016-09-23 09:21:33 +0200

    Add files via upload
    Added all the modules for a full working desktop app.

---
+++ game.py
@@ -0,0 +1,212 @@
+'''
+Created on 07/set/2016
+
+@author: Lorenzo Selvatici
+'''
+
+from agents import AgentRole
+from rules import Rules
+from settings import Settings
+
+
+
+class GameStateData:
+    """
+    Store all the data of a game state (GameState). 
+    It will be passed to the graphic object to initialize/update the GUI.
+    """
+    
+    def __init__(self, layout, agents):
+        """
+        Initialize the data of a game state: layout (see Layout.py), a list of agent (Agent in agents.py)
+        Mr. X in the agent at index 0. The remainder are the cops.
+        """
+        assert len(agents)>=2, "Too few agents!"
+        self.layout = layout
+        self.agents = agents
+    
+    def getNumMoves(self):
+        """
+        Returns the number of Mr.X moves.
+        """
+        return len(self.getAgentState(0).getMovesHistory())
+    
+    def getAgent(self, agentIndex):
+        """
+        Returns the agent at index agentIndex in self.agents.
+        Mr. X in the agent at index 0. The remainder are the cops.
+        """
+        assert 0 <= agentIndex < len(self.agents)
+        return self.agents[agentIndex]
+        
+    def getAgentState(self, agentIndex):
+        """
+        Returns the state of the agent at index agentIndex in self.agents.
+        Mr. X in the agent at index 0. The remainder are the cops.
+        """
+        return self.getAgent(agentIndex).getAgentState()
+    
+    def numberOfCops(self):
+        """
+        Returns the number of cops.
+        """
+        return len(self.agents) - 1
+    
+    def deepCopy(self):
+        """
+        Returns a copy of the Game State Data.
+        """
+        agents_copy = [a.deepCopy() for a in self.agents]
+        return GameStateData(self.layout, agents_copy)
+    
+    def __eq__(self, other):
+        return self.agents==other.agents and self.layout==other.layout
+    
+    def __str__(self):
+        return "\n\n".join([a.__repr__() for a in self.agents])
+
+class GameState:
+    """
+    A wrapper for GameStateData.
+    Modules outside of game.py should use it as the only way to collect information of the state of the game.
+    """
+    def __init__(self, data):
+        """
+        Initializes the game state with the supplied instance of GameStateData.
+        """
+        self.data = data
+    
+    def getNumMoves(self):
+        """
+        Returns the number of Mr.X moves.
+        """
+        return len(self.data.getAgentState(0).getMovesHistory())
+    
+    def deepCopy(self):
+        """
+        Returns a copy of itself.
+        """
+        return GameState(self.data.deepCopy())
+    
+    def getCopPosition(self, copIndex = None):
+        """
+        Returns the position of the cop at the specified index.
+        The index should be an integer in the interval [1, numberOfCops].
+        """
+        if copIndex is not None:
+            return self.data.getAgentState(copIndex).getPosition()
+        else:
+            return [self.getCopPosition(i+1) for i in range(self.numberOfCops())]
+    
+    def getCopState(self, copIndex):
+        """
+        Returns the state of the cop at the specified index.
+        The index should be an integer in the interval [1, numberOfCops].
+        """
+        return self.data.getAgentState(copIndex)
+    
+    def numberOfCops(self):
+        """
+        Returns the number of cops.
+        """
+        return self.data.numberOfCops()
+
+    def getLegalCopActions(self, copIndex):
+        """
+        Returns the list of the legal actions of the cop at the specified index.
+        The index should be an integer in the interval [1, numberOfCops].
+        """
+        return Rules.getLegalActions(self.data.getAgent(copIndex), self.data)
+         
+    
+    def generateSuccessor(self, agentIndex, action):
+        """
+        Returns a new instance of GameState where the agent at the specified agentIndex has taken his action.
+        AgentIndex is an integer in the interval [0, numberOfCops]. Index 0 is Mr. X.
+        """
+        # assert it's not a final state
+        turn = AgentRole.Mr_X if agentIndex==0 else AgentRole.COP
+        assert not self.isEndState(turn)[0] # check the first boolean element of the tuple
+        
+        new_state = self.deepCopy()
+        agent = new_state.data.getAgent(agentIndex)
+        if not Rules.isLegalAction(agent, action, self.data):
+            raise Exception("Illegal action!" + action.__str__())
+        
+        agent.performAction(action)
+        return new_state
+    
+    def isEndState(self, turn):
+        """
+        Returns a tuple of 3 elements.
+        (True, AgentRole, String) if the game is over, AgentRole is the winner and String is a description of the state.
+        (False, None, "") if the game is NOT over.
+        
+        The input parameter turn (AgentRole) determines whose turn.
+        """
+        # check for an overlap between Mr.X position and the cops' ones
+        mrXposition = self._getMrXPosition()
+        copPositions = self.getCopPosition()
+        if mrXposition in copPositions:
+            return (True, AgentRole.COP, "Mr. X has been caught from COP n." + \
+                    str(copPositions.index(mrXposition)+1) + ".\nThe winners are the COPS.\n")
+        
+        # check if the the maximum number of moves have been reached
+        if self.getNumMoves() == Settings.getMaxNumMoves() and turn == AgentRole.Mr_X:
+            return (True, AgentRole.Mr_X, "Maximum number of moves reached (" + str(Settings.getMaxNumMoves()) + \
+                    ").\nThe winner is Mr. X.\n")
+        
+        # check if Mr.X has no legal moves available
+        if len(self._getLegalMrXActions())==0 and turn == AgentRole.Mr_X:
+            return (True, AgentRole.COP, "Mr. X can not move from his position " + \
+                    str(mrXposition) + ".\nThe winners are the COPS.\n")
+              
+        # check if the cops have run out of tickets (legalMoves)
+        if turn == AgentRole.COP:
+            for i in range(self.numberOfCops()):
+                if self.getLegalCopActions(i+1) != []:
+                    return (False, None, "")
+            return (True, AgentRole.Mr_X, "The cops can not move.\nThe winner is Mr. X.\n")
+        
+        return (False, None, "")
+    
+    def _getMrXPosition(self):
+        return self.data.getAgentState(0).getPosition()
+    
+    def _getLegalMrXActions(self):
+        return Rules.getLegalActions(self.data.getAgent(0), self.data)
+    
+    def __eq__(self, other):
+        return self.data == other.data
+        
+    def __repr__(self):
+        return self.data.__str__()    
+        
+if __name__ == '__main__':
+    from agents import  MrX, KeyboardMrX, KeyboardCop, Cop
+    agents_list = [KeyboardMrX()] + [KeyboardCop(i+1) for i in range(Settings.getNumberOfCops())]
+    layout = Settings.getLayout()
+    data = GameStateData(layout, agents_list)
+    
+    gameState = GameState(data)
+    print gameState
+    print "="*30
+    turn = AgentRole.Mr_X
+    counter=0
+    while gameState.isEndState(turn)[0]==False:
+        gameState = gameState.generateSuccessor(counter, gameState.data.getAgent(counter).getAction(gameState))
+        print "="*20
+        print gameState
+        print "="*30
+        counter+=1
+        counter = counter % len(agents_list)
+        turn = AgentRole.Mr_X if counter==0 else AgentRole.COP
+
+
+
+
+
+
+
+
+
---
+++ graphic/display.py
@@ -0,0 +1,275 @@
+'''
+Created on 12/set/2016
+
+@author: Lorenzo Selvatici
+'''
+
+import util
+import GUI_util as gu
+import pygame
+from settings import Settings
+from agents import AgentRole
+
+
+class Display:
+    """
+    A generic display.
+    """
+
+    def __init__(self, gameStateData):
+        """
+        Initializes a display given the data of a game state (GameStateData see game.py)
+        """
+        self.data = gameStateData
+
+    def start(self):
+        """
+        Starts the display.
+        """
+        util.raiseNotDefined()
+
+    def update(self, new_gameStateData):
+        """
+        Update the display with the new data supplied.
+        """
+        pass
+
+    def wait(self, ms=None):
+        """
+        Wait for a user event.
+        """
+        util.raiseNotDefined()
+
+    def showMessage(self, message):
+        """
+        Shows a message to the user
+        """
+        util.raiseNotDefined()
+
+    def finish(self):
+        """
+        Terminates the display.
+        """
+        util.raiseNotDefined()
+
+
+class ConsoleDisplay(Display):
+    """
+    ..
+    """
+
+    def __init__(self, gameStateData):
+        Display.__init__(self, gameStateData)
+
+    def start(self):
+        """
+        Starts the display.
+        """
+        self._refresh()
+
+    def update(self, new_gameStateData):
+        """
+        Update the display with the new data supplied.
+        """
+        self.data = new_gameStateData
+        self._refresh()
+
+    def finish(self):
+        """
+        Terminates the display.
+        """
+        pass
+
+    def wait(self, ms=None):
+        """
+        Wait for a user event.
+        """
+        raw_input("\n\nPress a key...")
+
+    def showMessage(self, message):
+        """
+        Shows a message to the user
+        """
+        print message
+
+    def askForAction(self, role, index, currPos, possActions):
+        """
+        Ask the user to perform an action.
+        """
+        print str(role) + " -> " + str(index)
+        print "Current position : " + str(currPos)
+        print "Possible actions : " + str(possActions)
+        request = raw_input("Destination, TicketType = ").split(',')
+        dest, ticket = int(request[0]), request[1].strip().upper()
+        return dest, ticket
+
+    def _refresh(self):
+        import os
+        clear = lambda: os.system('cls')
+        clear()
+        print "\n\n" + "=" * 40 + "\n"
+        print self.data
+        print "" + "=" * 40 + "\n"
+
+
+
+class GUIDisplay(Display):
+    """
+    ...
+    """
+
+    def __init__(self, gameStateData):
+        Display.__init__(self, gameStateData)
+        # screen
+        self.screen = pygame.display.set_mode(gu.SCREEN_SIZE)
+        pygame.display.set_caption("Scotland Yard")
+        # game board
+        self.board = pygame.Surface(gu.BOARD_SIZE)
+        self.board = self.board.convert()
+        self.board.fill(gu.BG_COLOR)
+        # info panel
+        self.infoPanel = pygame.Surface(gu.INFO_PANEL_SIZE)
+        self.infoPanel = self.infoPanel.convert()
+        self.infoPanel.fill(gu.INFO_PANEL_COLOR)
+        # settings panel
+        self.settingsPanel = pygame.Surface(gu.SETTINGS_PANEL_SIZE)
+        self.settingsPanel = self.settingsPanel.convert()
+        self.settingsPanel.fill(gu.SETTINGS_PANEL_COLOR)
+        # moves panel
+        self.movesPanel = pygame.Surface(gu.MOVES_PANEL_SIZE)
+        self.movesPanel = self.movesPanel.convert()
+        self.movesPanel.fill(gu.MOVES_PANEL_COLOR)
+        
+
+    def start(self):
+        """
+        Starts the display.
+        """
+        self._drawGameBoardAndInfo()
+        self.STATIC_BOARD = self.board.copy() # create a copy for later
+        self._drawPlayers()
+        self.screen.blit(self.board, gu.BOARD_RECT)
+        pygame.display.update()
+        self.wait(1000)
+
+    def update(self, new_gameStateData):
+        """
+        Update the display with the new data supplied.
+        """
+        self.data = new_gameStateData
+        
+        # update board
+        self.board = self.STATIC_BOARD.copy()
+        self._drawPlayers()
+        self.screen.blit(self.board, gu.BOARD_RECT)
+        # update settings panel
+        self.showMessage(gu.getSettingsPanelMessage(self.data), self.settingsPanel, 
+                         bg_color = gu.SETTINGS_PANEL_COLOR, 
+                         rect = gu.SETTINGS_PANEL_RECT)
+        # update Mr.X moves panel
+        self.showMessage(gu.getMovesPanelMessage(self.data), self.movesPanel, 
+                         bg_color = gu.MOVES_PANEL_COLOR, 
+                         rect = gu.MOVES_PANEL_RECT)
+        pygame.display.update()
+        
+
+    def wait(self, ms=None):
+        """
+        Wait for a user event.
+        """
+        if ms is None:  
+            while True: # I know it doesn't make sense, but do not remove it.
+                for _ in pygame.event.get():
+                    pass
+                return
+        else:        
+            pygame.time.wait(ms)
+
+
+    def waitQuit(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    return
+
+    def showMessage(self, message, surface=None, bg_color=None, rect=None):
+        """
+        Shows a message to the user, the default surface is INFO PANEL.
+        """
+        if surface is None:
+            surface = self.infoPanel        
+        if bg_color is None:
+            bg_color = gu.INFO_PANEL_COLOR
+        if rect is None:
+            rect = gu.INFO_RECT
+            
+        surface.fill(bg_color)  # clear from previous messages
+        
+        lines = message.split("\n")
+        font = pygame.font.Font(None, 25)
+        dy = 20
+        for i, line in enumerate(lines):
+            txt_surf = font.render(line, False, gu.WHITE)
+            new_rect = txt_surf.get_rect().move(0, i*dy)
+            surface.blit(txt_surf, new_rect)
+        
+        self.screen.blit(surface, rect)
+        self.wait()
+        pygame.display.update()
+
+    def askForAction(self, role, index, currPos, possActions):
+        """
+        Ask the user to perform an action.
+        """
+        message = str(role) + " -> " + str(index)
+        if role == AgentRole.COP or Settings.isDebug():
+            message += "\nCurrent position : " + str(currPos)
+            message += "\nPossible actions :\n   " + "\n   ".join([a.__repr__() for a in possActions])
+        self.showMessage(message, surface=self.infoPanel, bg_color=gu.INFO_PANEL_COLOR)
+        request = raw_input("Destination, TicketType = ").split(',')
+        dest, ticket = int(request[0]), request[1].strip().upper()
+        
+        return dest, ticket
+
+    def finish(self):
+        """
+        Terminates the display.
+        """
+        pygame.quit()
+
+    def _drawGameBoardAndInfo(self):
+        gu.drawEdges(self.board)
+        gu.drawNodes(self.board)
+        gu.drawNumbers(self.board)
+        
+        self.showMessage("Starting a new Game...", self.infoPanel, 
+                         bg_color = gu.INFO_PANEL_COLOR, 
+                         rect = gu.INFO_RECT)
+        self.showMessage(gu.getSettingsPanelMessage(self.data), self.settingsPanel, 
+                         bg_color = gu.SETTINGS_PANEL_COLOR, 
+                         rect = gu.SETTINGS_PANEL_RECT)
+        self.showMessage("Mr.X info", self.movesPanel, 
+                         bg_color = gu.MOVES_PANEL_COLOR, 
+                         rect = gu.MOVES_PANEL_RECT)
+
+    def _drawPlayers(self):
+        if Settings.isDebug() or self.data.getNumMoves() in Settings.getNotHiddenMovesNumbers():
+            gu.drawAgent(self.board, self.data.getAgent(0))
+        for i in range(self.data.numberOfCops()):
+            gu.drawAgent(self.board, self.data.getAgent(i+1))
+        
+        
+        
+if __name__ == '__main__':
+    #from agents import  MrX, KeyboardMrX, KeyboardCop, Cop
+    from game import GameStateData
+    from scotlandYard import createAgents
+    agents_list = createAgents()
+    print agents_list
+    layout = Settings.getLayout()
+    data = GameStateData(layout, agents_list)
+    dp = GUIDisplay(data)
+    dp.start()
+    dp.waitQuit()
